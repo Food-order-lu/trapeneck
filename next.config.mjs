8d4fdc4302668b59cc6b935a96131f727f2b1df6
Tiago Ribeiro <tiagoribeiro@MacBook-Pro-de-Tiago.local>
2025-11-28 17:01:36 +0100

    Update basePath to /trapeneck

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/am-trapeneck',
+  basePath: '/trapeneck',
   images: {
     unoptimized: true,
   },
